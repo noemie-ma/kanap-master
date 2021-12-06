012b2dd19cd134c7637adb0a7cfd58b66c6779ce
Noémie <mallet.noemie46@gmail.com>
2021-12-06 14:04:44 +0100

    fin de l'apparition des produits

--- back/js/script.js
+++ back/js/script.js
@@ -1,33 +1,43 @@
-let products;
-
 // API REQUEST
-const fetchProducts = async() => {
-  products = await fetch(
-    'http://127.0.0.1:3000/api/products').then(res =>
-    res.json());
-
-    console.log(products);
+const urlApi = "http://127.0.0.1:3000/api/products";
+const fetchProducts = async () => {
+  await fetch(urlApi)
+    .then((res) => {
+      console.log(res);
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((products) => {
+      console.log(products);
+      showProducts(products);
+    })
+    .catch((erreur) => {
+      console.log(erreur);
+      alert(
+        "Une erreur est survenue ! Veuillez contacter l'adiministrateur du site"
+      );
+    });
 };
+fetchProducts();
 
 // Showing products on the index page
-const showProducts = async() => {
-  await fetchProducts();
+function showProducts(produits) {
+  let html = "";
 
-  results.innerHTML = (
+  produits.forEach((product) => {
+    html =
+      html +
+      `
+    <a href="./product.html?id=${product._id}">
+      <article>
+        <img src="${product.imageUrl}" alt="${product.altTxt}" />
+        <h3 class="productName">${product.name}</h3>
+        <p class="productDescription">${product.description}</p>
+      </article>
+    </a>
+  `;
+  });
 
-    products
-      .map(product => (
-        `
-          <a href="./product.html?id=42">
-            <article>
-              <img src="${product.imageUrl}" alt="${product.altTxt}" />
-              <h3 class="productName">${product.name}</h3>
-              <p class="productDescription">${product.description}</p>
-            </article>
-          </a>
-        `
-      )).join('')
-  );
-};
-
-showProducts();
+  document.getElementById("items").innerHTML = html;
+}
