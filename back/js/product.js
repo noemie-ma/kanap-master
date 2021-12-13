e0f4f16c100350722e12d66c14d1a0d3230b6819
Noémie <mallet.noemie46@gmail.com>
2021-12-13 12:27:31 +0100

    Remodification de l'affichage des éléments + début lien localStorage

--- back/js/product.js
+++ back/js/product.js
@@ -1,25 +1,98 @@
 var searchUrl = new URLSearchParams(window.location.search);
 var getId = searchUrl.get("id");
 console.log(getId);
+const urlProduct = "http://127.0.0.1:3000/api/products/" + getId;
+loadProduct();
+function loadProduct() {
+  console.log(urlProduct);
+  fetch(urlProduct)
+    .then((response) => {
+      console.log(response);
+      if (response.ok) return response.json();
+    })
+    .then((product) => {
+      displayProduct(product);
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("Veuillez contacter l'admin");
+    });
+}
 
-(async function () {
-  const response = await fetch("http://127.0.0.1:3000/api/products/${getId}");
-  const product = await response.json();
+function displayProduct(product) {
+  console.log(product);
+  document.querySelector(
+    ".item__img"
+  ).innerHTML = ` <img src="${product.imageUrl}" alt="${product.altTxt}"> `;
 
-  document.getElementsByTagName("img")[0].src = product.imageUrl;
-  let test1 = document.querySelector(".item_img img");
-  test1 = product;
-  document.getElementsByTagName("img")[0].alt = product.altTxt;
-  document.getElementById("title").textContent = product.name;
-  document.getElementById("price").textContent = product.price;
-  document.getElementById("description").textContent = product.description;
-  document.getElementById("colors").textContent = product.colors;
+  document.getElementById("title").textContent = `${product.name}`;
+  document.getElementById("price").textContent = `${product.price}`;
+  document.getElementById("description").textContent = `${product.description}`;
 
-  let colors = product.colors;
-  for (let color of colors) {
+  for (let color of product.colors) {
     let option = document.createElement("option");
     option.value = color;
     option.innerText = color;
     document.getElementById("colors").appendChild(option);
   }
-})();
+  document
+    .getElementById("colors")
+    .addEventListener("change", function (event) {
+      event.stopPropagation();
+      event.preventDefault();
+      controleColor(this.value);
+    });
+
+  document
+    .getElementById("quantity")
+    .addEventListener("change", function (event) {
+      event.stopPropagation();
+      event.preventDefault();
+      controleQuantite(this.value);
+    });
+
+  document
+    .getElementById("addToCart")
+    .addEventListener("click", function (event) {
+      event.stopPropagation();
+      event.preventDefault();
+      let validQuantite = controleQuantite(
+        document.getElementById("quantity").value
+      );
+      let validColor = controleColor(document.getElementById("colors").value);
+      if (validQuantite && validColor) {
+        AjouterProduitAuPanier();
+      }
+    });
+}
+
+// controler la quantité
+function controleQuantite(quantite) {
+  let valid = true;
+  if (quantite < 1 || quantite > 100) {
+    valid = false;
+    alert("Veuillez choisir une quantite entre 1 et 100! ");
+  }
+  return valid;
+}
+
+function controleColor(color) {
+  let valid = true;
+  if (color == "") {
+    valid = false;
+    alert("Veuillez choisir une couleur !");
+  }
+  return valid;
+}
+
+function AjouterProduitAuPanier() {
+  console.log("je suis la ");
+}
+// Potit bout de local storage : pour ajouter un produit dans le locaStorage
+/* function addProductLocalStorage(productCart, productCartStorage) {
+	// ajout dans le tableau de l'objet avec la couleur choisie
+	productCartStorage.push(productCart);
+
+	// transforme le fichier en json et envoie la clé du produit dans le localstorage
+	localStorage.setItem('products', JSON.stringify(productCartStorage));
+} */
